e0ec3783c362cc5c19324b364df16d0ecb35f9f7
spborder <50718056+spborder@users.noreply.github.com>
2025-03-26 13:11:03 -0400

    Adding closed key to converted histomics/large-image annotations

--- src/fusion_tools/utils/shapes.py
+++ src/fusion_tools/utils/shapes.py
@@ -624,6 +624,7 @@ def geojson_to_histomics(geojson_anns: Union[list,dict]):
                     {
                         'type': 'polyline',
                         'user': f['properties'],
+                        'closed': True,
                         'points': [list(i)+[0] if type(i)==tuple else i+[0] for i in f['geometry']['coordinates'][0]]
                     }
                     for f in g['features']
